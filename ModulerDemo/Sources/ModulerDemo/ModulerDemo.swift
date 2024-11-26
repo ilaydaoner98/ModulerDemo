aa8faac45144fc660dc429f2fd2fab2644ae9cd2
ilaydaoner <ilayda.oner@flo.com.tr>
2024-11-26 13:58:57 +0300

    first push

---
+++ ModulerDemo/Sources/ModulerDemo/ModulerDemo.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+extension Int {
+    public func squareOfNumber(_ number: Int) -> Int {
+        return number * number
+    }
+    
+}
